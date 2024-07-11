c156dda80ec66ed7c45fa46e651a9ac70d881dcb
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-07-11 13:19:53 +0300

    Initial commit

---
+++ AnimalExplorer/AnimalExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalExplorerApp.swift
+//  AnimalExplorer
+//
+//  Created by Andrii Boichuk on 11.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimalExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
